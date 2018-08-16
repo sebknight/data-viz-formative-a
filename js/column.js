4aecd3ed14bb8342e6c3ce800ef69d559dbe6b2f
Katie elizabeth French <845855192@bun2-206-16.local>
2018-08-17 11:33:25 +1200

    gets val of checked checbox in column filter

--- js/column.js
+++ js/column.js
@@ -5,20 +5,20 @@ function drawChart(){
 
     const data = google.visualization.arrayToDataTable([
         ["Class" , "Number of Students"],
-        ["2013" , 58719],
-        ["2014" , 57243],
-        ["2015" , 61038],
-        ["2016" , 59430]
+        ["Web & UX" , 18],
+        ["Film" , 9],
+        ["Game Design" , 12],
+        ["Graphic Design" , 7]
     ]);
 
     //the options
     const options = {
         title: "Number of students in each class in 2018",
         hAxis: {
-            title: "Number of Students"
+            title: "Class"
         },
         vAxis: {
-            title: "Class"
+            title: "Number of Students"
         },
         animation: {
             startup: true,
@@ -30,3 +30,19 @@ function drawChart(){
     const chart = new google.visualization.ColumnChart(document.getElementById("columnContainer"));
     chart.draw(data, options);
 }
+
+//an empty gloabl array that will be used to store the names of the selected classes
+var columnClasses = [];
+$( "#columnFilterSbmt" ).click(function() {
+    event.preventDefault();
+    //find the checkboxes
+    $('input:checkbox[name=columnClass]').each(function(){
+        //do this for every checkbox that is checked
+        if($(this).is(":checked")){
+            //grab the name of the checkbox and push to collumnclasses array
+            columnClasses.push($(this).val());
+            console.log($(this).val());
+        }
+    });
+
+});
