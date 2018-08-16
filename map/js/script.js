f516dc2bc603a209dce89fd162ec3c7953b6fedf
sebknight <37192621+sebknight@users.noreply.github.com>
2018-08-17 03:48:32 +1200

    times working

--- map/js/script.js
+++ map/js/script.js
@@ -1,49 +1,189 @@
 console.log('script ready');
+console.log(morning);
+// console.log(midday);
+// console.log(afternoon);
+
+// function initMap() {
+//     var directionsService = new google.maps.DirectionsService();
+//     var directionsDisplay = new google.maps.DirectionsRenderer();
+//     var yoobee = new google.maps.LatLng(-41.279113, 174.780283);
+//     var mapOptions = {
+//         zoom: 13,
+//         center: yoobee
+//     }
+//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//     directionsDisplay.setMap(map);
+//     function calcRoute() {
+//         for (let i = 0; i < tripData.suburbs.length; i++) {
+//             var start = tripData.suburbs[i];
+//             // var arrival = morning;
+//             var request = {
+//                 origin: start,
+//                 destination: yoobee,
+//                 travelMode: tripData.transport,
+//                 transitOptions: {
+//                     arrivalTime: morning
+//                 }
+//             };
+//         }
+//         directionsService.route(request, function (result, status) {
+//             if (status == 'OK') {
+//                 directionsDisplay.setDirections(result);
+//             }
+//         });
+//     }
+//     calcRoute();
+
+// }
+
+// initMap();
+
+
+
+// function initMap() {
+//     var yoobee = new google.maps.LatLng(-41.279113, 174.780283);
+//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//     var mapOptions = {
+//         zoom: 13,
+//         center: yoobee
+//     }
+//         var directionsService = new google.maps.DirectionsService();
+//         var directionsDisplay = new google.maps.DirectionsRenderer();
+//         directionsDisplay.setMap(map);
+//         function calcRoute() {
+//             for (let i = 0; i < tripData.suburbs.length; i++) {
+//                 //gets origin
+//                 var start = tripData.suburbs[i];
+//                 var request = {
+//                     origin: start,
+//                     destination: yoobee,
+//                     //sets travel mode
+//                     travelMode: tripData.transport,
+//                     transitOptions: {
+//                         //sets time - allow filter?
+//                         arrivalTime: morning
+//                     }
+//                 };
+//             }
+//             directionsService.route(request, function (result, status) {
+//                 if (status == 'OK') {
+//                     directionsDisplay.setDirections(result);
+//                 }
+//             });
+//         }
+
+
+//         calcRoute();
+
+
+//     }
+//     // var directionsService = new google.maps.DirectionsService();
+//     // var directionsDisplay = new google.maps.DirectionsRenderer();
+//     // var yoobee = new google.maps.LatLng(-41.279113, 174.780283);
+//     // var mapOptions = {
+//     //     zoom: 13,
+//     //     center: yoobee
+//     // }
+//     // directionsDisplay.setMap(map);
+//     // function calcRoute() {
+//     //     for (let i = 0; i < tripData.suburbs.length; i++) {
+//     //         var start = tripData.suburbs[i];
+//     //         // var arrival = morning;
+//     //         var request = {
+//     //             origin: start,
+//     //             destination: yoobee,
+//     //             travelMode: tripData.transport,
+//     //             transitOptions: {
+//     //                 arrivalTime: morning
+//     //             }
+//     //         };
+//     //     }
+//     //     directionsService.route(request, function (result, status) {
+//     //         if (status == 'OK') {
+//     //             directionsDisplay.setDirections(result);
+//     //         }
+//     //     });
+//     // }
+//     // calcRoute();
+
+
+// initMap();
+
+// function getClassTime(){
+//     var classTime = [this];
+// }
+
+var suburbForm = document.getElementById('suburb-form');
+var classTimeForm = document.getElementById('time-form');
+// var classTimeName = classTimeForm.value;
+
+// var classTime = [classTimeForm.value];
+
+// for (let i = 0; i < classTimeForm.length; i++) {
+//     var classTimeName = classTimeForm[i].value;
+//     var classTime = times[i].classTimeName;    
+// }
+// function getClassTime(){
+
+
+
 
 function initMap() {
-     var directionsService = new google.maps.DirectionsService;
-     var directionsDisplay = new google.maps.DirectionsRenderer;
-        var mapOptions = {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var yoobee = new google.maps.LatLng(-41.279113, 174.780283);
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: {
-            lat: -41.286686,
-            lng: 174.777945
-        },
-        disableDoubleClickZoom: true,
-        disableDefaultUI: true,
-        zoomControl: false,
-        fullscreenControl: false
-        //  streetViewControl: true,
-        //  streetViewControlOptions: {
-        //      position: google.maps.Control.Position.LEFT_TOP
-        //  }
-        //styling goes here             
-    }; //mapOptions ENDS
-    //creates map using mapOptions
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        center: yoobee
+    });
+    
+    
+    directionsDisplay.setMap(map);
 
-}
+    var onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    suburbForm.addEventListener('change', onChangeHandler);
+    classTimeForm.addEventListener('change', onChangeHandler);
+
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var classTimeName = classTimeForm.value;
+        console.log(classTimeName);
+        var classTime = times.classTimeName;       
+        var suburbName = suburbForm.value;
+        console.log(suburbName);
+        var suburb = suburbList.suburbName;
+        console.log(suburb);
+        
+        var transportMode = suburbList.suburb.transport;
+        console.log(transportMode);
+        
+        
+            
+    
+            directionsService.route({
+                origin: suburb,
+                destination: yoobee,
+                // travelMode: transportMode,
+                transitOptions: {
+                    arrivalTime: classTime
+                }
+            },
+      
+
+                function (response, status) {
+                    if (status === 'OK') {
+                        directionsDisplay.setDirections(response);
+                    } else {
+                        window.alert('Directions request failed due to ' + status);
+                    }
+                });
+            
+        } //calculateAndDisplayRoute ENDS
+
+        } //initMap ENDS
+
+
 
 initMap();
-     directionsDisplay.setMap(map);
-
-     var onChangeHandler = function () {
-         calculateAndDisplayRoute(directionsService, directionsDisplay);
-     };
-     document.getElementById('start').addEventListener('change', onChangeHandler);
-     document.getElementById('end').addEventListener('change', onChangeHandler);
- }
-
- function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-     directionsService.route({
-         origin: document.getElementById('start').value,
-         destination: document.getElementById('end').value,
-         travelMode: 'DRIVING'
-     }, function (response, status) {
-         if (status === 'OK') {
-             directionsDisplay.setDirections(response);
-         } else {
-             window.alert('Directions request failed due to ' + status);
-         }
-     });
- }
---
+++ map/js/suburbs.js
@@ -0,0 +1,91 @@
+// var morning = new Date();
+// morning.setHours(8);
+// morning.setMinutes(30);
+// console.log(morning);
+var morning = new Date();
+morning.setHours(20);
+morning.setMinutes(30);
+
+var midday = new Date();
+midday.setHours(24);
+midday.setMinutes(0);
+
+var afternoon = new Date();
+afternoon.setHours(3);
+afternoon.setMinutes(30);
+
+// var times = [morning, midday, afternoon]
+
+var times = {
+    "morning": morning,
+    "midday": midday,
+    "afternoon": afternoon
+}
+
+
+// var suburbs = {
+
+// }
+
+
+// var suburbs = [{
+    
+//     start: 'Aro Valley, Wellington',
+//     end: 'Yoobee School of Design, Wellington',
+//             transport: 'TRANSIT'
+//     //             drivingOptions: {
+//     //     morning: new Date();  // for the time N milliseconds from now.
+//     //     afternoon: new Date();
+//     //     evening: new Date();//     trafficModel: 'optimistic'
+//     // }
+// }]
+
+
+// var journey = [{
+var suburbList = {
+        "aro valley, wellington": {
+            // start: 'Aro Valley, Wellington',
+            transport: 'TRANSIT'
+        },
+        wainui: {
+            start: 'Wainuiomata, Lower Hutt',
+            transport: 'TRANSIT'
+        },
+        evans: {
+            start: 'Evans Bay, Wellington',
+            transport: 'TRANSIT'
+        },
+        island: {
+            start: 'Island Bay, Wellington',
+            transport: 'TRANSIT'
+        },
+        grenada: {
+            start: 'Grenada Village, Wellington',
+            transport: 'TRANSIT'
+        },
+        newtown: {
+            start: 'Newtown, Wellington',
+            transport: 'TRANSIT'
+        },
+        newlands: {
+            start: 'Newlands, Wellington',
+            transport: 'TRANSIT'
+        },
+        lowerHutt: {
+            start: 'Lower Hutt, Wellington',
+            transport: 'TRANSIT'
+        }
+
+    }
+
+// var tripData = {
+//     suburbs: ["Aro Valley, Wellington", "Wainuiomata, Lower Hutt"],
+//     transport: "TRANSIT"
+
+// }
+    //             drivingOptions: {
+    //     morning: new Date();  // for the time N milliseconds from now.
+    //     afternoon: new Date();
+    //     evening: new Date();//     trafficModel: 'optimistic'
+    // }
+// }]
